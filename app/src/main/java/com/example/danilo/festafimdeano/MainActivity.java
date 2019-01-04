29f6ce3e60099cdb5e33b2a8abd918bfecfd3dde
Danilo Araújo <daniloaraujo1990@hotmail.com>
2019-01-03 23:44:11 -0300

    Commit View

--- app/src/main/java/com/example/danilo/festafimdeano/MainActivity.java
+++ app/src/main/java/com/example/danilo/festafimdeano/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.danilo.festafimdeano;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private ViewHolder mViewHolder = new ViewHolder();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textToday = (TextView) findViewById(R.id.text_today);
+        this.mViewHolder.textDayLeft = (TextView) findViewById(R.id.text_day_left);
+        this.mViewHolder.textToday = (Button) findViewById(R.id.button_confirm);
+
+        this.mViewHolder.buttonConfirm.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.button_confirm) {
+            Intent intent = new Intent(this, DetailsActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private static class ViewHolder {
+        TextView textToday;
+        TextView textDayLeft;
+        Button buttonConfirm;
     }
 }
